7238bb86e989144db7d0d9cb96cf7deee7e3dd47
carons02 <simon.j.caron@gmail.com>
2025-07-26 00:10:00 -0400

    upload package 4

---
+++ SquareAreaFormula/squareareaformula.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareAreaFormula
+{
+    public class squareareaformula
+    {
+        public int Processchoice(int length, int area)
+        {
+            area = length * length;
+            return area;
+        }
+    }
+}
---
+++ TriangleAreaDLL/TriangleAreaFormula.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleAreaDLL
+{
+    public class TriangleAreaFormula
+    {
+        //create the choice selection DLL which is sent the variables created by the user,
+        //inputted into the selected formula, then return the resulting value to the main form
+        public decimal Processchoice(decimal baselength, decimal height, decimal area)
+        {
+            //takes the respective variables, solves the equation, then returns the value to the main form
+            area = (baselength * height) / 2;
+            return area;
+        }
+    }
+}
